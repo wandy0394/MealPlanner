1b3f49cb52a49d5cbd0fa2600d8917210ab0031f
Dev <wandy0394@gmail.com>
2022-11-13 17:11:53 +1100

    added api call to backend

--- frontend/meal-planner/src/components/SearchIngredients.js
+++ frontend/meal-planner/src/components/SearchIngredients.js
@@ -1,11 +1,23 @@
 import { TextField, Button, Box } from "@mui/material";
-
+import DataService from "../service/data-service"
 
 export default function SearchIngredients() {
+    
+    const handleClick= (async () => {
+        console.log('Clicked')
+        try {
+            const output = await DataService.searchIngredients('apple')
+            console.log(output)
+        }
+        catch (e) {
+            console.error(e)
+        }
+        
+    })
     return (
         <Box>
             <TextField label='Search by name..' variant='standard'></TextField> 
-            <Button variant='contained'>Search Ingredients</Button>
+            <Button variant='contained' onClick={handleClick}>Search Ingredients</Button>
         </Box>
     )
 }
