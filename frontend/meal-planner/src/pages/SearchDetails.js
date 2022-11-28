be9fd098c1951653768fd9dde09b49fe1b32df9c
Dev <wandy0394@gmail.com>
2022-11-28 22:26:17 +1100

    recipe details page now fetches from api on mount

--- frontend/meal-planner/src/pages/SearchDetails.js
+++ frontend/meal-planner/src/pages/SearchDetails.js
@@ -1,15 +1,38 @@
-import { Box } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { ContentBox } from "../components/ContentBox";
+import DataService from "../service/data-service";
 
 export default function SearchDetails(props) {
     const params = useParams()
-    return (
-        <Box>
-            Hello
-            {
-                params.id
-            }
+    const [recipe, setRecipe] = useState({})
 
-        </Box>
+    useEffect(()=> {
+        getRecipe()
+    }, [])
+    async function getRecipe() {
+        try {
+            const data = await DataService.getRecipe(params.id)
+            console.log(data)    
+        }
+        catch (e) {
+            console.error('error')
+        }
+    }
+
+    return (
+        <ContentBox sx={{height:'100%'}}>
+            <Stack sx={{height:'100%'}}>
+                <Box sx={{border:'solid'}}>
+                    <Paper elevation={3}>
+                        <Typography variant='h2' sx={{margin:'1rem auto', textAlign:'center', border:'none'}}>
+                            Recipe Details
+                        </Typography>
+                    </Paper>
+                </Box>
+
+            </Stack>
+        </ContentBox>
     )
 }
