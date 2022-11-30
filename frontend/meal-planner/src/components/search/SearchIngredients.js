d9644b1b68ff7b2507a5ced0826e1874b1687956
Dev <wandy0394@gmail.com>
2022-11-30 22:11:44 +1100

    removed unusued components

--- frontend/meal-planner/src/components/search/SearchIngredients.js
+++ frontend/meal-planner/src/components/search/SearchIngredients.js
@@ -1,5 +1,7 @@
-import { TextField, Button, Box, FormGroup, FormLabel } from "@mui/material";
-
+import { TextField, Button, Box, FormGroup, FormLabel, Typography, Pagination } from "@mui/material";
+import DataService from "../../service/data-service";
+import SearchIngredientResults from "./SearchIngredientsResults";
+import {ACTION_TYPES} from './ActionTypes'
 const dummyOutput = {
 
     food: [
@@ -13,37 +15,83 @@ const dummyOutput = {
 
 }
 
-export default function SearchIngredients({searchText, setSearchText, handleClickSearch}) {
+export default function SearchIngredients({state, dispatch}) {
 
     
-    
+    async function callSearchIngredients(query, page, doStoreSearch) {
+        try {
+            console.log(`useffect ${page}` )
+            const callSearch = (async() => {
+                const data = await DataService.searchIngredients(query, (page-1), doStoreSearch)
+                console.log('Got Data')
+                console.log(data)
+                dispatch({type:ACTION_TYPES.SET_PREV_QUERY, payload:state.query})
+                dispatch({type:ACTION_TYPES.SET_QUERY, payload:query})
+                dispatch({type:ACTION_TYPES.SET_RESULTS, payload:data})
+                dispatch({type:ACTION_TYPES.SET_PAGE, payload:page})
+                console.log(state)
+            })();
+        }
+        catch(e) {
+            console.error(e)
+        }        
+    }
+
     async function handleClick(e) {
         e.preventDefault()
         console.log('clicked')
-        //console.log(`Page ${page}`)
-        //setPage(1)
-        const call = (async() => {await handleClickSearch(searchText, 1, true)})()
+        await callSearchIngredients(state.query, 1, true)
     }
 
+    async function handleIngredientPageChange(e, page) {
+        console.log(`page change ${page}`)
+        dispatch({type:ACTION_TYPES.SET_PAGE, payload:page})
+        await callSearchIngredients(state.prevQuery, page, false)
+    }
 
     function handleChange(e) {
-        setSearchText(e.target.value)
+        dispatch({type:ACTION_TYPES.SET_QUERY, payload:e.target.value})
     }
 
     return (
         <Box>
-            <form onSubmit={handleClick}>
+            <Box>
+                <form onSubmit={handleClick}>
 
+            
+                <FormGroup>
+                    <FormLabel sx={{marginBottom: '1rem', marginTop:'3rem'}}>What ingredients do you want to find?</FormLabel>
+                    <Box sx={{display:'flex', flexDirection:'column', padding:'0px 0rem'}}>
+                        <TextField sx={{marginBottom:'1em'}}label='Search by name..' variant='standard' onChange={handleChange} required={true} value={state.query}></TextField> 
+                    </Box>
+                    <Button variant='contained' type='submit'>Search Ingredients</Button>
+                </FormGroup>
+                </form>
 
-            <FormGroup>
-                <FormLabel sx={{marginBottom: '1rem', marginTop:'3rem'}}>What ingredients do you want to find?</FormLabel>
-                <Box sx={{display:'flex', flexDirection:'column', padding:'0px 0rem'}}>
-                    <TextField sx={{marginBottom:'1em'}}label='Search by name..' variant='standard' onChange={handleChange} required={true} value={searchText}></TextField> 
-                </Box>
-                <Button variant='contained' type='submit'>Search Ingredients</Button>
-            </FormGroup>
-            </form>
-
+            </Box>
+            <Box>
+                    <Typography variant='h4' sx={{display:'inline'}}>
+                                Results
+                    </Typography>
+                {
+                    (state.results !== null) && (
+                        (<Typography variant='h6' sx={{display:'inline'}}> (Total Results: {state.results.total_results})</Typography>)
+                    )
+                }
+            </Box>
+            <Box>
+                {
+                    ((state.results !== null) && <><SearchIngredientResults data={state.results}/></>)
+                }
+            </Box>
+            <Box>
+                {
+                    ((state.results !== null) && 
+                    <Pagination count={10} shape='rounded' sx={{margin: '1rem 0'}} page={state.page}
+                        onChange={handleIngredientPageChange}
+                    />)
+                }
+            </Box>
         </Box>
     )
 }
