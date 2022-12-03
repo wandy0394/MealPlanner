da89ce5b5fdf9078e160bd68c72c53aa11adec47
Dev <wandy0394@gmail.com>
2022-12-03 22:06:20 +1100

    re-styled RecipeContent. Edit feature todo

--- frontend/meal-planner/src/components/recipe/RecipeContent.js
+++ frontend/meal-planner/src/components/recipe/RecipeContent.js
@@ -1,11 +1,14 @@
 import { Box, Button, FormControl, FormGroup, Grid, InputLabel, MenuItem, Paper, Select, TextField, Typography } from "@mui/material";
 import SaveIcon from '@mui/icons-material/Save'
+import EditIcon from '@mui/icons-material/Edit'
 import { Stack } from "@mui/system";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import DataService from "../../service/data-service";
 import Nutrition from "./Nutrition";
 import IngredientsPane from "./IngredientsPane";
 import RecipeNameInput from "./RecipeNameInput";
+import {UnitConverter} from "../utility/Units"
+import { ACTION_TYPES } from "./ActionTypes";
 
 const MAX_CHARS = 4000
 const INITIAL_MACROS = {
@@ -14,127 +17,194 @@ const INITIAL_MACROS = {
     protein:0,
     calories:0
 }
+const INITIAL_RECIPE = {
+    title:'',
+    instructions:'',
+    ingredients:{},
+    macros:INITIAL_MACROS,
+    counter:0,
+    servings:0,
+    prepTime:0,
+    cookTime:0
+}
+const calculator = new UnitConverter()
+
 export default function RecipeContent({storedInstructions, storedTitle, storedRecipeIngredients, storedMacros}) {
-    const [instructions, setInstructions] = useState(storedInstructions)
-    const [title, setTitle] = useState(storedTitle)
-    const [ingredients, setIngredients] = useState([])
-    const [recipeIngredients, setRecipeIngredients] = useState({})
-    const [ingredientCounter, setIngredientCounter] = useState(0)
-    const [macros, setMacros] = useState(storedMacros)
+    const [recipe, dispatch] = useReducer(reducer, INITIAL_RECIPE)
+    const [readOnly, setReadOnly] = useState(true)
 
-
-
-    useEffect(()=>{
-        getIngredients()
-        setIngredientCounter(storedRecipeIngredients.length)
-        setRecipeIngredients(storedRecipeIngredients)
-        //setMacros(storedMacros)
-        // console.log(units)
-    },[])
+    function reducer (state, action) {
+        const {type, payload} = action
+        switch(type) {
+            case ACTION_TYPES.SET_TITLE:
+                return {...state, title:payload}
+            case ACTION_TYPES.SET_MACROS:
+                return {...state, macros:payload}
+            case ACTION_TYPES.SET_INSTRUCTIONS:
+                return {...state, instructions:payload}
+            case ACTION_TYPES.SET_COUNTER:
+                return {...state, counter:payload}
+            case ACTION_TYPES.SET_INGREDIENTS:
+                return {...state, ingredients:payload}
+            case ACTION_TYPES.UPDATE_INGREDIENTS:
+                return {...state, ingredients:{...state.ingredients, [payload.id]:payload.data}}
+            case ACTION_TYPES.INCREMENT_COUNTER:
+                return{...state, counter: state.counter + 1}
+            case ACTION_TYPES.ADD_INGREDIENT:
+                return {...state, ingredients:{...state.ingredients, [state.counter]:payload}, counter:state.counter+1} 
+            case ACTION_TYPES.DELETE_INGREDIENT:
+                const newIngredients = {...state.ingredients}
+                delete newIngredients[payload]
+                return {...state, ingredients:newIngredients}    
+            case ACTION_TYPES.UPDATE_QTY:
+                return {...state, 
+                    ingredients:{
+                        ...state.ingredients, 
+                        [payload.id]:{
+                            ...state.ingredients[payload.id], 
+                            qty:payload.data
+                        }
+                    }
+                }
+            case ACTION_TYPES.UPDATE_UNIT:
+                return {...state, 
+                    ingredients:{
+                        ...state.ingredients, 
+                        [payload.id]:{
+                            ...state.ingredients[payload.id], 
+                            unit:payload.data
+                        }
+                    }
+                }
+            case ACTION_TYPES.SET_SERVINGS:
+                return {...state, servings:payload}
+            case ACTION_TYPES.SET_PREP_TIME:
+                return {...state, prepTime:payload}
+            case ACTION_TYPES.SET_COOK_TIME:
+                return {...state, cookTime:payload}
+            default:
+                return state
+        }
+    }
 
     useEffect(()=> {
-        setTitle(storedTitle)
-        setInstructions(storedInstructions)
-        setRecipeIngredients(storedRecipeIngredients)
-        setIngredientCounter(storedRecipeIngredients.length)
-        setMacros(storedMacros)
+        const newMacros = {
+            carbs:parseFloat(calculator.calculateCarbs(recipe.ingredients)).toFixed(2),
+            fat:parseFloat(calculator.calculateFat(recipe.ingredients)).toFixed(2),
+            protein:parseFloat(calculator.calculateProtein(recipe.ingredients)).toFixed(2),
+            calories:parseFloat(calculator.calculateCalories(recipe.ingredients)).toFixed(2)
+        }
+        dispatch({type:ACTION_TYPES.SET_MACROS, payload: newMacros})
+    }, [recipe.ingredients])
+
+    useEffect(()=> {
+        dispatch({type:ACTION_TYPES.SET_TITLE, payload:storedTitle})
+        dispatch({type:ACTION_TYPES.SET_INGREDIENTS, payload:storedRecipeIngredients})
+        dispatch({type:ACTION_TYPES.SET_INSTRUCTIONS, payload:storedInstructions})
+        dispatch({type:ACTION_TYPES.SET_MACROS, payload:storedMacros})
+        
+        //setIngredientCounter(storedRecipeIngredients.length)
+        
     }, [storedInstructions, storedTitle, storedRecipeIngredients, storedMacros])
 
-    useEffect(()=> {
-        console.log(recipeIngredients)
-        
-    }, [recipeIngredients])
-
-    function setupRecipeIngredients() {
-        const arr = storedRecipeIngredients.map((item, index) => {
-            return {
-                [index]:{...item, id:item.id}
-            }
-        })
-        setIngredientCounter(storedRecipeIngredients.length)
-        return arr
-    }
-    
-
-    async function getIngredients() {
-        try {
-            console.log('Refreshing Ingredients')
-            const result = await DataService.getIngredients()
-            //console.log(result)
-            setIngredients(result)
-        }
-        catch (e) {
-            console.error(e)
-        }
-    }
-
     function handleInstructionChange(e) {
-        setInstructions(e.target.value)
+        dispatch({type:ACTION_TYPES.SET_INSTRUCTIONS, payload:e.target.value})
     }
 
-    function handleTitleChange(e) {
-        setTitle(e.target.value)
+    function handleEditClick(e) {
+        console.log('edit')
+        setReadOnly((prev) => !prev)
     }
 
     async function handleSaveClicked(e) {
         e.preventDefault();
         const data = {
-            title:title,
-            ingredients:recipeIngredients,
-            instructions:instructions,
-            macros:macros
+            title:recipe.title,
+            ingredients:recipe.ingredients,
+            instructions:recipe.instructions,
+            macros:recipe.macros
         }
-        const result = await DataService.addRecipe(data)
+        console.log(data)
+        setReadOnly((prev) => !prev)
+        console.log('save')
     }
 
 
     return (
         <Stack>
             <form onSubmit={handleSaveClicked}>
-                <RecipeNameInput
-                    title={title}
-                    setTitle={setTitle}
-                    isDisabled={true}
-                />
-            
-                <Grid container spacing={2} sx={{padding:'1rem', border:''}}>
-
-                    <Grid item xs={12} md={6} sx={{width:'100%'}}>
-                        <IngredientsPane 
-                            recipeIngredients={recipeIngredients}
-                            setRecipeIngredients={setRecipeIngredients}
-                            ingredientCounter={ingredientCounter}
-                            setIngredientCounter={setIngredientCounter}
-                            ingredients={ingredients} 
-                            isDisabled={true}
-                            readOnly={true}
+                <Grid container spacing={3} sx={{padding:'1rem', border:''}}>
+                    <Grid item sm={12} md={6}>
+                        <RecipeNameInput
+                            title={recipe.title}
+                            prepTime={recipe.prepTime}
+                            cookTime={recipe.cookTime}
+                            servings={recipe.servings}
+                            dispatch={dispatch}
                         />
-                        <Nutrition foods={recipeIngredients} macros={macros} setMacros={setMacros} doCalculate={false}/>
                     </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Box>
-                            
-                            <TextField variant='outlined' label='Instructions' 
-                                multiline 
-                                required 
-                                sx={{width:'100%'}}
-                                rows={4}
-                                value={instructions}
-                                onChange = {handleInstructionChange}
-                                inputProps={{maxLength:MAX_CHARS}}
-                                disabled 
-                            >
-                                
-                            </TextField>
-                            <Typography>Count: {instructions.length}/{MAX_CHARS}</Typography>
-                        </Box>
-                        <Paper elevation={3}>
-                            <Typography variant='h6'>Add an Image</Typography> 
-                        </Paper>
+                    <Grid item sm={12} md={6}>
+                            <Nutrition macros={recipe.macros} />
                     </Grid>
                     
+
+                    <Grid item sm={12} md={6} sx={{width:'100%'}}>
+                        <IngredientsPane 
+                            recipeIngredients={recipe.ingredients}
+                            dispatch={dispatch}
+                            isDisabled={false}
+                            readOnly={readOnly}
+                        />
+                    </Grid>
+
+                    <Grid item sm={12} md={6} >
+                        <Paper elevation={3} sx={{height:'50vh', padding:'1rem'}}>
+                            <Box sx={{display:'flex', flexDirection:'column', gap:'1rem'}}>
+                                <Box sx={{display:'flex', alignItems:'center',gap:'1rem'}}>
+                                    <Typography variant='h6'>Instructions</Typography>
+                                    <Typography variant='body'> {recipe.instructions.length}/{MAX_CHARS}</Typography> 
+                                </Box>
+                            
+                                <TextField variant='standard' label='Write your instructions here' 
+                                        multiline 
+                                        required 
+                                        sx={{width:'100%', height:'90%'}}
+                                        maxRows={20}
+                                        value={recipe.instructions}
+                                        onChange = {handleInstructionChange}
+                                        inputProps={{maxLength:MAX_CHARS}}
+                                    >
+                                    
+                                </TextField>
+                            </Box>
+                        </Paper>    
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <Box sx={{dsplay:'flex'}}>
+                            {
+                             readOnly ? ( 
+                                <Button variant='contained' sx={{width:'100%'}} onClick={handleEditClick}>
+                                    <EditIcon/>
+                                    <Typography variant='body' sx={{padding:'0 1rem'}}>Edit</Typography>
+                                </Button>   
+                             ) : null
+                                
+                            }
+                            {
+                                readOnly ? (
+                                    null
+                                ) : (
+                                    <Button variant='contained' type='submit' sx={{width:'100%'}}>
+                                        <SaveIcon/>
+                                        <Typography variant='body' sx={{padding:'0 1rem'}}>Save</Typography>
+                                    </Button>
+                                )
+                            }
+  
+                        </Box>
+                    </Grid>
                 </Grid>
+
             </form>
         </Stack>
     )
