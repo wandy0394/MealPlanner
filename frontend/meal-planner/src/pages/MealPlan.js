9d82ed25fda005493e0abeb03b15a0de5f401309
Dev <wandy0394@gmail.com>
2022-12-12 21:33:10 +1100

    basic skeleton psuedocode added

--- frontend/meal-planner/src/pages/MealPlan.js
+++ frontend/meal-planner/src/pages/MealPlan.js
@@ -1,5 +1,6 @@
 import AddBox from "@mui/icons-material/AddBox";
 import { Paper, Stack, Box, Typography } from "@mui/material";
+import CreateMealForm from "../components/meals/CreateMealForm";
 
 import { ContentBox } from "../components/utility/ContentBox";
 export default function MealPlans() {
@@ -8,6 +9,9 @@ export default function MealPlans() {
             <Stack sx={{height:'100%'}}>
                 <Box sx={{flexGrow:'1'}}>
                     <Typography variant='h3' sx={{margin:'1rem auto', textAlign:'left', border:'none'}}>What do you plan to eat?</Typography>
+                    <CreateMealForm/>
+
+                    
                 </Box>
             </Stack>
         </ContentBox>
