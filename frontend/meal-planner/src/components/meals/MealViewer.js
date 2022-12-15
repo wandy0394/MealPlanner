9dbfb795af4edb3bdc63f6950847170307c3392d
Dev <wandy0394@gmail.com>
2022-12-15 16:39:27 +1100

    implemented getMeals in backend. Need to display it

--- frontend/meal-planner/src/components/meals/MealViewer.js
+++ frontend/meal-planner/src/components/meals/MealViewer.js
@@ -1,7 +1,37 @@
 import {Box} from "@mui/material"
+import { useEffect, useState } from "react"
+import DataService from "../../service/data-service"
 
+const useGetMeals = () => {
+    const [meals, setMeals] = useState({})
+    let counter=0
+    let called = false
+    useEffect(()=> {        
+        if (!called) {
+            async function fetchData() {
+                try {
+                    const result = await DataService.getMeals()
+                    console.log(result)
+                    // setMeals(result)
+                }
+                catch (e) {
+
+                }
+            }
+            fetchData();
+        }
+        return () =>{
+            called = true
+        }
+    })
+
+
+
+    return [meals, setMeals]
+}
 
 export default function MealViewer() {
+    const [meals, setMeals] = useGetMeals()
     
     //api call to get meals belonging to this user
 
@@ -11,7 +41,7 @@ export default function MealViewer() {
             
             {/* view meals for each day of the week */}
             {/* each meal describes the recipes/ingredients and macros and targert macros */}
-
+            
         </Box>
     )
 }
--- frontend/meal-planner/src/pages/MealPlan.js
+++ frontend/meal-planner/src/pages/MealPlan.js
@@ -1,19 +1,35 @@
 import AddBox from "@mui/icons-material/AddBox";
-import { Paper, Stack, Box, Typography } from "@mui/material";
+import { Paper, Stack, Box, Typography, Modal, Button } from "@mui/material";
+import { useState } from "react";
 import CreateMealForm from "../components/meals/CreateMealForm";
+import MealViewer from "../components/meals/MealViewer";
 
 import { ContentBox } from "../components/utility/ContentBox";
 export default function MealPlans() {
+    const [open, setOpen] = useState(false)
+
+    function handleClickOpen() {
+        setOpen(true)
+    }
+    function handleClose() {
+        setOpen(false)
+    }
     return (
         <ContentBox sx={{height:'100%'}}>
             <Stack sx={{height:'100%'}}>
                 <Box sx={{flexGrow:'1'}}>
                     <Typography variant='h3' sx={{margin:'1rem auto', textAlign:'left', border:'none'}}>What do you plan to eat?</Typography>
-                    <CreateMealForm/>
-
-                    
+                    <Button onClick={handleClickOpen}>Add</Button>
+                    <MealViewer/>
                 </Box>
             </Stack>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                sx={{display:'flex', alignItems:'center', justifyContent:'center'}}
+            >
+                <CreateMealForm/>
+            </Modal>
         </ContentBox>
     )
 }
