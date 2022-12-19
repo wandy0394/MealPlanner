32f97fded8a41b4ce0bfcecbfacec877dcf82189
Dev <wandy0394@gmail.com>
2022-12-19 21:37:37 +1100

    removed unused module

--- backend/data/recipe.js
+++ backend/data/recipe.js
@@ -325,39 +325,6 @@ export default class Recipe {
             return {error: 'could not get authentication token'}
         }
     }
-
-    // static async searchRecipes(searchText) {
-    //     try {
-    //         const token = await TokenHandler.getToken();
-    //         const params = new URLSearchParams();
-    //         params.append('method', "recipes.search.v2")
-    //         params.append('search_expression', searchText.toString())
-    //         params.append('format', "json")
-    //         params.append('max_results', 10)
-
-    //         const options = {
-    //             method: 'POST',
-    //             headers: {
-    //                 'content-type': 'application/x-www-form-urlencoded',
-    //                 'Authorization': `Bearer ${token}`
-    //             },
-    //             body: params
-    //         }
-    //         try {
-    //             const res = await fetch('https://platform.fatsecret.com/rest/server.api', options);
-    //             const resJSON = await res.json();
-    //             console.log(resJSON)
-    //             return resJSON;
-    //         }
-    //         catch (e) {
-    //             console.log(e)
-    //             return {error:`error fetching from api: ${e.message}`}
-    //         }
-    //     }
-    //     catch {
-    //         return {error: 'could not get authentication token'}
-    //     }
-    // }
     static async searchRecipesWithData(searchData) {
         try {
             const token = await TokenHandler.getToken();
