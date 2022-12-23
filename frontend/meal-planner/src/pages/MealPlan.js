9783f502da4088606fb19b1b6142675e7914b757
Dev <wandy0394@gmail.com>
2022-12-23 15:35:24 +1100

    standardised layouts across all pages

--- frontend/meal-planner/src/pages/MealPlan.js
+++ frontend/meal-planner/src/pages/MealPlan.js
@@ -1,11 +1,13 @@
 import AddBox from "@mui/icons-material/AddBox";
-import { Paper, Stack, Box, Typography, Modal, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { Paper, Stack, Box, Typography, Modal, Button, IconButton } from "@mui/material";
 import { useState } from "react";
 import CreateMealForm from "../components/meals/CreateMealForm";
 import MealList from "../components/meals/MealList";
 import MealViewer from "../components/meals/MealViewer";
 import { useGetAllFood, useGetMeals } from "../components/meals/utility/MealItemUtil";
 import { ContentBox } from "../components/utility/ContentBox";
+import MainPane from "../layouts/MainPane";
 
 export default function MealPlans() {
     const [open, setOpen] = useState(false)
@@ -19,22 +21,26 @@ export default function MealPlans() {
         setOpen(false)
     }
     return (
-        <ContentBox sx={{height:'100%'}}>
-            <Stack sx={{height:'100%'}}>
-                <Box sx={{flexGrow:'1'}}>
-                    <Typography variant='h3' sx={{margin:'1rem auto', textAlign:'left', border:'none'}}>What do you plan to eat?</Typography>
-                    <Button onClick={handleClickOpen}>Add</Button>
+        <MainPane
+            title='What do you plan to eat?'
+            buttons={
+                <>
+                    <IconButton onClick={handleClickOpen}><AddIcon/></IconButton>
+                </>
+            }
+            mainContent={
+                <>
                     <MealList mealSets={mealSet} mealItems={mealItems}/>
-                    {/* <MealViewer mealSet={mealSet} mealItems={mealItems}/> */}
-                </Box>
-            </Stack>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                sx={{display:'flex', alignItems:'center', justifyContent:'center'}}
-            >
-                <CreateMealForm mealItems={mealItems}/>
-            </Modal>
-        </ContentBox>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        sx={{display:'flex', alignItems:'center', justifyContent:'center'}}
+                    >
+                        <CreateMealForm mealItems={mealItems}/>
+                    </Modal>
+                </>
+            }
+        >
+        </MainPane>
     )
 }
