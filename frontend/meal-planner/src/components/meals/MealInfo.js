f22c49738bcd1f2ceef835a8c63b048b6cb0bf23
Dev <wandy0394@gmail.com>
2022-12-24 13:43:53 +1100

    refactored MealInfo

--- frontend/meal-planner/src/components/meals/MealInfo.js
+++ frontend/meal-planner/src/components/meals/MealInfo.js
@@ -1,41 +1,55 @@
 import { Box, Typography } from "@mui/material"
 
+function Row({title, total,target, unit}) {
+    return (
+        <>
+            <Typography variant="body1" sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', paddingRight:'2rem'}}>{title}</Typography>
+            <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
+                <Typography variant="h4" display='inline' sx={{color:'goldenrod'}}>{total}</Typography>
+                <Typography variant="body1" display='inline'>/{target}{unit}</Typography>
+            </Box>
+        </>
+    )
+}
+
 export default function MealInfo(props) {
     const {totalCalories, totalProtein, totalCarbs, totalFat, targetCalories, targetProtein, targetFat, targetCarbs} = props
 
     return (
-        <Box sx={{display:'grid', gridTemplateColumns:'1fr 1fr', gridTemplateRows:'1fr 1fr 1fr 1fr 1fr', color:'white', justifyContent:'center', alignContent:'center'}}>
+        <Box sx={{display:'grid', gridTemplateColumns:'1fr 1fr', gridTemplateRows:'repeat(5, 1fr)', color:'white', justifyContent:'center', alignContent:'center'}}>
 
-            <Typography variant="body1" sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', paddingRight:'2rem'}}></Typography>
-            <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-                <Typography variant="h4" display='inline' sx={{color:'goldenrod'}}>Total</Typography>
-                <Typography variant="body1" display='inline'>/Target</Typography>
-            </Box>
+            <Row
+                title=''
+                total='Total'
+                target='Target'
+            />
 
-                <Typography variant="body1" sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', paddingRight:'2rem'}}>Calories</Typography>
-            <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-                <Typography variant="h4" display='inline' sx={{color:'goldenrod'}}>{totalCalories}</Typography>
-                <Typography variant="body1"display='inline'>/{targetCalories}kcal</Typography>
+            <Row
+                title='Calories'
+                total={totalCalories}
+                target={targetCalories}
+                unit='kcal'
+            />
 
-            </Box>
-            <Typography variant="body1" sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', paddingRight:'2rem'}}>Carbs</Typography>
-            <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-                <Typography variant="h4" display='inline'sx={{color:'goldenrod'}}>{totalCarbs}</Typography>
-                <Typography variant="body1" display='inline'>/{targetCarbs}g</Typography>
-            </Box>
-            
-            
-            <Typography variant="body1" sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', paddingRight:'2rem'}}>Fat</Typography>
-            <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-                <Typography variant="h4" display='inline'sx={{color:'goldenrod'}}>{totalFat}</Typography>
-                <Typography variant="body1" display='inline'>/{targetFat}g</Typography>
-            </Box>
+<           Row
+                title='Carbs'
+                total={totalCarbs}
+                target={targetCarbs}
+                unit='g'
+            />
+            <Row
+                title='Fat'
+                total={totalFat}
+                target={targetFat}
+                unit='g'
+            />
+            <Row
+                title='Protein'
+                total={totalProtein}
+                target={targetProtein}
+                unit='g'
+            />
 
-            <Typography variant="body1" sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', paddingRight:'2rem'}}>Protein</Typography>
-            <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-                <Typography variant="h4" display='inline'sx={{color:'goldenrod'}}>{totalProtein}</Typography>
-                <Typography variant="body1" display='inline'>/{targetProtein}g</Typography>
-            </Box>
         </Box>
     )
 }
