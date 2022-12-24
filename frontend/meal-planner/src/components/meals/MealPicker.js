c008f93ac6dd64d1061bbbe3cf64bd2b0b5437fd
Dev <wandy0394@gmail.com>
2022-12-24 17:14:27 +1100

    removed unused code

--- frontend/meal-planner/src/components/meals/MealPicker.js
+++ frontend/meal-planner/src/components/meals/MealPicker.js
@@ -1,6 +1,6 @@
 import { Box, Button, Typography } from "@mui/material"
 import { ACTION_TYPES } from "./ActionTypes"
-import MealCard, { StaticMealcard } from "./MealCard"
+import MealCard from "./MealCard"
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from '@mui/icons-material/Remove'
 
@@ -15,10 +15,7 @@ export default function MealPicker(props) {
                     return (
                         // <Box sx={{display:'flex', alignItems:'center', justifyContent:'center'}}>
                         <Box key={index}    sx={{display:'grid', gridTemplateColumns:'3fr 1fr'}}>
-                            {
-                                <MealCard key={index} id={key} meal={item} image={item.image}/>
-                                //(item.type === 'custom') ? <MealCard key={index} id={key} meal={item}/> : <StaticMealcard key={index} id={key} meal={item}/>
-                            }
+                            <MealCard key={index} id={key} meal={item} image={item.image}/>
                             <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', height:'100%', gap:'2rem', padding:'1rem'}}>
                                 <Typography variant='h4'>{meals[key]?.qty !== undefined ? meals[key]?.qty : 0 }</Typography>
                                 <Button variant='contained' sx={{aspectRatio:'1/1'}} onClick={e=>handleIncrement(key, item)}><AddIcon/></Button>
